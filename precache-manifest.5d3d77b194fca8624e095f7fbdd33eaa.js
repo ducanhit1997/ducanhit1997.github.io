398a6a1bc7cb2521eccd70b496fc2c602b65de76
PhongNL <phongnl@splus-software.com.vn>
2020-03-20 21:24:26 +0700

    Build CV v1.0

---
+++ precache-manifest.5d3d77b194fca8624e095f7fbdd33eaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29663facc2a6a6ded51f92dc39ad6310",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e36abde68bf061a31639",
+    "url": "/static/css/2.2e872fcd.chunk.css"
+  },
+  {
+    "revision": "948d288f6e2b618deda2",
+    "url": "/static/css/main.2ba1ace8.chunk.css"
+  },
+  {
+    "revision": "e36abde68bf061a31639",
+    "url": "/static/js/2.35b86073.chunk.js"
+  },
+  {
+    "revision": "948d288f6e2b618deda2",
+    "url": "/static/js/main.f3fa86f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b8b34e5499cbfb45adf9e088d03deac3",
+    "url": "/static/media/anh.b8b34e54.jpg"
+  }
+]);
